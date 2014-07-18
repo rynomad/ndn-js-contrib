917a20c50f11c93932be22539c66d857faf2b791
Ryan Bennett <nomad.ry@gmail.com>
2014-07-17 21:37:17 -0600

    add full mocha suite.js

---
+++ test/node/suite.js
@@ -0,0 +1,5 @@
+
+var cs = require("./DataStructures/ContentStore.js")
+var fib = require("./DataStructures/FIB.js")
+var pit = require("./DataStructures/PIT.js")
+var nt = require("./DataStructures/NameTree.js")
