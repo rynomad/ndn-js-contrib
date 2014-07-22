3be2924bc8c5a3ad6f68367649f1ef6bedf32929
Ryan Bennett <nomad.ry@gmail.com>
2014-07-22 13:02:33 -0600

    alias browser transports and fix paths in index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 exports.ndn = require("ndn-lib");
-exports.NameTree = require("./src/NameTree.js").installNDN(exports.ndn);
-exports.PIT = require("./src/PIT.js").installNDN(exports.ndn);
-exports.FIB = require("./src/FIB.js").installNDN(exports.ndn);
-exports.ContentStore = require("./src/ContentStore.js")
-exports.Interfaces = require("./src/Interfaces.js").installNDN(exports.ndn);
-
+exports.NameTree = require("./src/DataStructures/NameTree.js").installNDN(exports.ndn);
+exports.PIT = require("./src/DataStructures/PIT.js").installNDN(exports.ndn);
+exports.FIB = require("./src/DataStructures/FIB.js").installNDN(exports.ndn);
+exports.ContentStore = require("./src/DataStructures/ContentStore.js");
+exports.Interfaces = require("./src/DataStructures/Interfaces.js").installNDN(exports.ndn);
+exports.Transports = require("./src/Transports/nodeWrapper.js");
 
 module.exports = exports;
