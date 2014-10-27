0b02047e7079c0111cbe7541ce45e635a8184824
Ryan Bennett <nomad.ry@gmail.com>
2014-10-27 00:19:03 -0600

    add debug file

---
+++ debug.js
@@ -0,0 +1,9 @@
+exports.ndn = require("ndn-js");
+exports.NameTree = require("./src/DataStructures/NameTree.js").installNDN(exports.ndn);
+exports.PIT = require("./src/DataStructures/PIT.js").installNDN(exports.ndn);
+exports.FIB = require("./src/DataStructures/FIB.js").installNDN(exports.ndn);
+exports.ContentStore = require("./src/DataStructures/ContentStore.js");
+exports.Interfaces = require("./src/DataStructures/Interfaces.js").installNDN(exports.ndn);
+exports.Transports = require("./src/Transports/node/export.js");
+
+module.exports = exports;
