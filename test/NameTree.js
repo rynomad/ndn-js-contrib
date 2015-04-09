e70b93597068a61ed411fe88fce5113dd3566d3b
Ryan Bennett <nomad.ry@gmail.com>
2015-04-09 15:30:17 -0600

    auto commit: Thu Apr 09 2015 15:30:17 GMT-0600 (MDT)

--- test/NameTree.js
+++ test/NameTree.js
@@ -506,12 +506,12 @@ describe('NameTree', function(){
           tree.insert(new NameTree.Node(new ndn.Name([""+i,'5','5','super','long','prefix','mmmk','word','can','i','narrow','it'])))
 
         function skip(node){
-          if (node.depth + node.prefix.size() < 1000)
+          if (node.depth + node.prefix.size() < 12)
             return true
           else
             return false;
         }
-        
+
 
         tree.skip(skip)
         var i = 0;
@@ -519,7 +519,7 @@ describe('NameTree', function(){
           console.log(node.prefix.toUri(), i)
           i++;
         }
-        assert(i === 0, 'if this is first err' + i)
+        assert(i === 121, 'if this is first err' + i)
 
       })
     })
