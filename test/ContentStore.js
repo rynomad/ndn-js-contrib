cc1eac86bf2906e6f46ea8117b0f420adc9aa057
Ryan Bennett <nomad.ry@gmail.com>
2015-04-12 13:20:41 -0600

    auto commit: Sun Apr 12 2015 13:20:41 GMT-0600 (MDT)

--- test/ContentStore.js
+++ test/ContentStore.js
@@ -136,18 +136,24 @@ describe("ContentStore", function(){
     })
 
 
-    it("should resolve rightMost with Exclude", function(){
+    it("should resolve rightMost with Exclude", function(done){
       var exSucc = new ndn.Data(new ndn.Name("test/interest/lookup/8"), "ExSUCCESS");
       cs.insert(exSucc)
-        .then(function(){
-          return cs.insert(left);
-        })
         .then(function(){
           var interest = new ndn.Interest(new ndn.Name("test/interest/lookup"));
           interest.setChildSelector(1);
           interest.setMustBeFresh(false);
-          interest.setExclude(new Exclude(['9']))
+          interest.setExclude(new ndn.Exclude([new ndn.Name.Component('9')]))
           return cs.lookup(interest);
+        })
+        .then(function(data){
+          if (data.content.toString() == "ExSUCCESS")
+            done()
+          else
+            assert(false, "did not exclude properly" + data.name.toUri())
+        }).catch(function(er){
+          console.log(er, er.stack)
+          assert(false);
         })
     })
 
