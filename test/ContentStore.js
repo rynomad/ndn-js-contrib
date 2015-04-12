dc825ed68f94957e11695aa6eee715b07f1ced47
Ryan Bennett <nomad.ry@gmail.com>
2015-04-12 13:26:58 -0600

    auto commit: Sun Apr 12 2015 13:26:58 GMT-0600 (MDT)

--- test/ContentStore.js
+++ test/ContentStore.js
@@ -218,7 +218,14 @@ describe("ContentStore", function(){
     })
 
     it("should reject for maxSuffix < shortest data", function(){
-
+      var interest = new ndn.Interest(new ndn.Name("test/interest/lookup"));
+      interest.setMaxSuffixComponents(0)
+      cs.lookup(interest)
+      .then(function(data){
+        assert(false, "returned false match" + data.name.toUri())
+      }).catch(function(er){
+        done()
+      })
     })
 
     it("should reject for mustBeFresh and stale content",function(){
