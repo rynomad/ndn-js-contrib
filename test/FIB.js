6ee48d55760fb7081e3b1394fa4dd6c23f37df00
Ryan Bennett <nomad.ry@gmail.com>
2015-04-13 00:10:21 -0600

    auto commit: Mon Apr 13 2015 00:10:21 GMT-0600 (MDT)

--- test/FIB.js
+++ test/FIB.js
@@ -42,6 +42,7 @@ describe("FIB", function(){
 
   describe("lookup(interest, face)", function(){
     var fib = new FIB()
+    var f1 = new ndn.Face(), f2 = new ndn.Face(), f3 = new ndn.Face()
     it("should return a promise",function(done){
       fib.lookup(new ndn.Interest(new ndn.Name("test/interest/fib/lookup")))
          .then(function(results){
@@ -51,6 +52,22 @@ describe("FIB", function(){
          })
     })
 
+    it("should resolve with all matches", function(){
+      fib.insert(new ndn.Name("test/insert/f1"), f1)
+         .then(function(){
+           return fib.insert(new ndn.Name("test/insert/f1/f2"), f2)
+         }).then(function(){
+           return fib.insert(new ndn.Name("test/insert/f1/f2/f3"), f3)
+         }).then(function(){
+           return fib.lookup(new ndn.Interest(new ndn.Name("test/insert/f1/f2/f3/term")))
+         }).then(function(results){
+           assert(results.length === 3)
+           assert(results[0].face === f1)
+           assert(results[1].face === f2)
+           assert(results[2].face === f3)
+           done()
+         })
+    })
   })
 
 })
