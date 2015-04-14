13452c3cf865ca73ac6cb33f271139b6893fbc39
Ryan Bennett <nomad.ry@gmail.com>
2015-04-14 14:16:40 -0600

    auto commit: Tue Apr 14 2015 14:16:40 GMT-0600 (MDT)

--- src/DataStructures/Repository.js
+++ src/DataStructures/Repository.js
@@ -187,7 +187,7 @@ Repository.prototype.destroy = function Repository_destroy(){
       return reject(new Error("Repository.destroy(): Repository must call Repository.close() prior to destruction"))
 
     leveldown.destroy(self._dataPath, function Repository_destroy_level(err){
-      console.log("leveldown.destroy")
+      console.log("leveldown.destroy", self._dataPath, err)
       if (err)
         return reject(err);
 
