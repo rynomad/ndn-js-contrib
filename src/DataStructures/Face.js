759b4c3324583d26f03983a0eaa4f535bc9a61b0
Ryan Bennett <nomad.ry@gmail.com>
2015-04-21 01:00:00 -0600

    auto commit: Tue Apr 21 2015 01:00:00 GMT-0600 (MDT)

--- src/DataStructures/Face.js
+++ src/DataStructures/Face.js
@@ -21,7 +21,7 @@ Face.Create = function Face_create(transport, onElement){
           }
         };
 
-    var face = new Face(settings);
+    face = new Face(settings);
 
     if (onElement)
       face.set_onReceivedElement(onElement);
